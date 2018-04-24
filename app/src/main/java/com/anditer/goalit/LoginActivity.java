c44028f41374cf06a1cb528c8f264c635ed1e134
awalmubarak <awalmubarak6@gmail.com>
2018-04-24 04:56:53 +0000

    Initial commit with working google sign in button

---
+++ app/src/main/java/com/anditer/goalit/LoginActivity.java
@@ -0,0 +1,89 @@
+package com.anditer.goalit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity{
+    GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+
+    private void updateUI(GoogleSignInAccount account) {
+
+        if (account!=null){
+            Log.i("LoginAccount", "NotNull");
+            Log.i("email", account.getEmail());
+            Log.i("name", account.getDisplayName());
+            Log.i("ID", account.getId());
+            Log.i("token", String.valueOf(account.getIdToken()));
+        }else{
+            Log.i("LoginAccount", "Null");
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==RC_SIGN_IN && resultCode==RESULT_OK){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            updateUI(account);
+
+        }catch (ApiException e){
+            e.printStackTrace();
+            updateUI(null);
+        }
+    }
+}
---
+++ app/src/main/java/com/anditer/goalit/utils/PrefManager.java
@@ -0,0 +1,39 @@
+package com.anditer.goalit.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * This class deals with our shared preferences
+ */
+
+public class PrefManager {
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private Context _context;
+
+    // shared pref mode
+    private int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "goalit-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
