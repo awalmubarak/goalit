5efdf1d7e30ccbc8efd14e81f80dee04fbb51282
awalmubarak <awalmubarak6@gmail.com>
2018-04-24 13:12:09 +0000

    Added Tasks and Goal Objects

---
+++ app/src/main/java/com/anditer/goalit/POJOs/Goal.java
@@ -0,0 +1,55 @@
+package com.anditer.goalit.POJOs;
+
+import java.util.ArrayList;
+
+/**
+ * This represents a goal object
+ */
+
+public class Goal {
+    private String title;
+    private Boolean status;
+    private ArrayList<Task> tasks;
+    private String addedDate;
+    private String endDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public String getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(String addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
---
+++ app/src/main/java/com/anditer/goalit/POJOs/Task.java
@@ -0,0 +1,26 @@
+package com.anditer.goalit.POJOs;
+
+/**
+ * Task objects
+ */
+
+public class Task {
+    String title;
+    Boolean status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
