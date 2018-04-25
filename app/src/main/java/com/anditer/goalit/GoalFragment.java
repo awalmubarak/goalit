96972ea823b5fe5f771dc544a2803fa9ea77debb
awalmubarak <awalmubarak6@gmail.com>
2018-04-25 19:12:48 +0000

    Added Goal Adapter and And Configured Goal Fragment to work with Goal Recycler View

--- app/src/main/java/com/anditer/goalit/GoalFragment.java
+++ app/src/main/java/com/anditer/goalit/GoalFragment.java
@@ -3,16 +3,36 @@ package com.anditer.goalit;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.anditer.goalit.POJOs.Goal;
+import com.anditer.goalit.adapters.GoalAdapter;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GoalFragment extends Fragment {
+    private ArrayList<Goal> goalList = new ArrayList<>();
+    private RecyclerView goalRecyclerView;
+    private LinearLayoutManager layoutManager;
+    private GoalAdapter goalAdapter;
 
+    private void populateGoalListWithGoals(){
+        for (int i=0;i<10;i++){
+            Goal goal = new Goal();
+            goal.setTitle("Become the Best Software Developer Ever " + String.valueOf(i));
+            goal.setEndDate("454");
+            goal.setAddedDate("32");
+            goalList.add(goal);
+        }
+    }
 
     public GoalFragment() {
         // Required empty public constructor
@@ -23,7 +43,15 @@ public class GoalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal, container, false);
+        View view = inflater.inflate(R.layout.fragment_goal, container, false);
+        populateGoalListWithGoals();
+        goalRecyclerView = view.findViewById(R.id.goalRecyclerView);
+        layoutManager = new LinearLayoutManager(getContext());
+        goalRecyclerView.setLayoutManager(layoutManager);
+        goalAdapter = new GoalAdapter(getContext(), goalList);
+        goalRecyclerView.setAdapter(goalAdapter);
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/anditer/goalit/adapters/GoalAdapter.java
@@ -0,0 +1,59 @@
+package com.anditer.goalit.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.anditer.goalit.POJOs.Goal;
+import com.anditer.goalit.R;
+
+import java.util.ArrayList;
+
+public class GoalAdapter extends RecyclerView.Adapter<GoalAdapter.GoalHolder>{
+    private ArrayList<Goal> goalsList;
+    private Context context;
+
+    public GoalAdapter(Context context, ArrayList<Goal> goals){
+        goalsList = goals;
+        this.context = context;
+    }
+
+
+    @Override
+    public GoalHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.goal_item, parent, false);
+        return new GoalHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(GoalHolder holder, int position) {
+        Goal goal = goalsList.get(position);
+        holder.goalTitle.setText(goal.getTitle());
+        holder.daysLeft.setText("Days Left: " + goal.getEndDate());
+        holder.taskAdded.setText("Tasks Added: " + goal.getAddedDate());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (goalsList.size()>0){
+            return goalsList.size();
+        }
+        return 0;
+    }
+
+    class GoalHolder extends RecyclerView.ViewHolder{
+        private TextView goalTitle, taskAdded, daysLeft;
+
+        public GoalHolder(View itemView) {
+            super(itemView);
+            goalTitle = itemView.findViewById(R.id.goalTitle);
+            taskAdded = itemView.findViewById(R.id.tasksAdded);
+            daysLeft = itemView.findViewById(R.id.daysLeft);
+        }
+    }
+
+}
